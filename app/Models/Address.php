62891c3b00c0816d8c0cd7b9595650c63619e94e
Shanalotte028 <aldemark28@gmail.com>
2024-10-10 08:30:33 +0800

    Add client ID to fillable to address

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -10,6 +10,7 @@ class Address extends Model
     use HasFactory;
     protected $table = 'addresses';
     protected $fillable = [
+        'client_id',
         'address_line_1',
         'address_line_2',
         'city',
