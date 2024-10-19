edeac58aef4c72327dc9b742a4868ad386d01f8b
Shanalotte028 <aldemark28@gmail.com>
2024-10-20 04:06:52 +0800

    Client Side Pages Whitespace Fixed

--- resources/views/components/client/section.blade.php
+++ resources/views/components/client/section.blade.php
@@ -1,8 +1,8 @@
-<section id="{{ $id }}" class="about section dark-background">
+<section id="{{ $id ?? '' }}" class="about section dark-background">
 
     <!-- Section Title -->
     <div class="container section-title" data-aos="fade-up">
-      <h2>{{ $title }}</h2>
+      <h2>{{ $title ?? '' }}</h2>
     </div><!-- End Section Title -->
 
     <div class="container" data-aos="fade-up" data-aos-delay="100">
