327b99d986f6d58e1ce63c317e478c95ddc2c6db
Shanalotte028 <aldemark28@gmail.com>
2025-01-31 01:46:37 +0800

    Add role in display table on User List

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -12,6 +12,7 @@
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Email</th>
+                        <th>Role</th>
                         <th>Created At</th>
                         <th>Updated At</th>
                     </tr>
@@ -23,6 +24,7 @@
                                 <td>{{ $user->first_name }}</td>
                                 <td>{{ $user->last_name }}</td>
                                 <td>{{ $user->email }}</td>
+                                <td>{{ $user->role}}</td>
                                 <td>{{ $user->created_at }}</td>
                                 <td>{{ $user->updated_at }}</td>
                             </tr>
