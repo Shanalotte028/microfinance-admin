1ac806e97323d258509e65eaa285d47de73e661f
Shanalotte028 <aldemark28@gmail.com>
2025-02-04 03:25:21 +0800

    CSS Improved

--- resources/views/components/admin/nav-bar.blade.php
+++ resources/views/components/admin/nav-bar.blade.php
@@ -2,23 +2,23 @@
     <!-- Navbar Brand-->
     <a class="navbar-brand ps-3" href="{{ route('dashboard') }}">Start Bootstrap</a>
     <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 text-light" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
         <div class="input-group">
         </div>
     <!-- Navbar-->
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
         <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+            <a class="nav-link dropdown-toggle text-light" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
             <ul class="dropdown-menu dropdown-menu-end bg-dark" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item text-muted" href="{{ route('admin.profile') }}">Profile</a></li>
-                <li><a class="dropdown-item text-muted" href="{{ route('admin.settings') }}">Settings</a></li>
-                <li><a class="dropdown-item text-muted" href="{{ route('admin.activity-log') }}">Activity Log</a></li>
+                <li><a class="dropdown-item" href="{{ route('admin.profile') }}">Profile</a></li>
+                <li><a class="dropdown-item" href="{{ route('admin.settings') }}">Settings</a></li>
+                <li><a class="dropdown-item" href="{{ route('admin.activity-log') }}">Activity Log</a></li>
                 <li><hr class="dropdown-divider" /></li>
                 <li>
                     <form action="{{ route('admin.logout') }}" method="POST">
                     @csrf
-                    <button class="dropdown-item text-muted" type="submit">Logout</button></li>
+                    <button class="dropdown-item" type="submit">Logout</button></li>
                 </form>
             </ul>
         </li>
