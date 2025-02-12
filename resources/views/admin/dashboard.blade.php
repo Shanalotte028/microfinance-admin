a7d0d6a23ee7fd9d5426e33629d2d961f2487eee
Shanalotte028 <aldemark28@gmail.com>
2025-02-13 07:20:45 +0800

    Added Appropriate Icons

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
         @can('compliances.index')
         <x-admin.cards 
-        icon="bi bi-journal"
+        icon="bi bi-hourglass-split"
         value="{{ $pendingCompliance }}"
         heading="Pending Compliances"
         route="{{ route('admin.compliances', ['status' => 'pending']) }}" 
@@ -12,14 +12,14 @@
         @can('legal.index')
         @if(Auth::user()->role === "Lawyer")
         <x-admin.cards 
-        icon="bi bi-bank2"
+        icon="bi bi-folder-check"
         value="{{ $assignedCases }}"
         heading="Assigned cases"
         route="{{ route('admin.legal.index') }}" 
         />
         @else
         <x-admin.cards 
-        icon="bi bi-bank2"
+        icon="bi bi-journal-text"
         value="{{ $openCase }}"
         heading="Open Cases"
         route="{{ route('admin.legal.index', ['status' => 'open']) }}" 
--- resources/views/components/admin/nav-side.blade.php
+++ resources/views/components/admin/nav-side.blade.php
@@ -4,22 +4,22 @@
             <div class="sb-sidenav-menu">
                 <div class="nav">
                     <a class="nav-link" href="{{ route('dashboard') }}">
-                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                        <div class="sb-nav-link-icon"><i class="bi bi-house-door"></i></div>
                         Home
                     </a>
                     @can('clients.index')
                     <div class="sb-sidenav-menu-heading">Client Management</div>
-                    <x-admin.nav-item id="collapseClients" heading="Clients" label="List Clients" route="{{ route('admin.client.index') }}" icon="fas fa-columns" />
+                    <x-admin.nav-item id="collapseClients" heading="Clients" label="List Clients" route="{{ route('admin.client.index') }}" icon="bi bi-people" />
                     @endcan
                     @can('compliances.index')
                     <div class="sb-sidenav-menu-heading">Compliance Management</div>
-                    <x-admin.nav-item id="collapseCompliances" heading="Compliances"  label="Compliance Records" route="{{ route('admin.compliances') }}" icon="fas fa-columns" />
+                    <x-admin.nav-item id="collapseCompliances" heading="Compliances"  label="Compliance Records" route="{{ route('admin.compliances') }}" icon="bi bi-file-earmark-check" />
                     @endcan
                     @can('users.index')
                     <div class="sb-sidenav-menu-heading">User Management</div>
                     <!-- Dropdown menu for User Management -->
                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseAccount" aria-expanded="false" aria-controls="collapseAccount">
-                        <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                        <div class="sb-nav-link-icon"><i class="bi bi-person-gear"></i></div>
                         User Management
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                     </a>
@@ -39,7 +39,7 @@
                     @can('legal.index')
                     <div class="sb-sidenav-menu-heading">Legal Management</div>
                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLegal" aria-expanded="false" aria-controls="collapseLegal">
-                        <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                        <div class="sb-nav-link-icon"><i class="bi bi-file-earmark-text"></i></div>
                         Legal Management
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                     </a>
