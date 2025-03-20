764ff7f1a4ac24582a2bb33eb9d7721b2974db24
Shanalotte028 <aldemark28@gmail.com>
2025-03-20 16:28:35 +0800

    Add a link to dashboard when receiving error 500

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,6 +3,10 @@
             <div class="text-center mt-4">
                 <h1 class="display-1 text-light">500</h1>
                 <p class="lead text-light">Oops! Something went wrong on our end. Please try again later or contact support.</p>
+                <a href="{{route('login')}}" class="text-muted">
+                    <i class="fas fa-arrow-left me-1"></i>
+                    Return to Dashboard
+                </a>
             </div>
         </div>
 </x-admin.auth-layout>
